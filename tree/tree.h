15325dd124e8b00a9500749694d58c6a0a7a3bb4
Matashkov Artyom <artemker667@gmail.com>
2026-03-13 10:55:17 +0300

    True postOrder

--- tree/tree.h
+++ tree/tree.h
@@ -19,8 +19,6 @@ Node *createNode(int value);
 void addNode(Node *parent, Node *newNode);
 
 // Нельзя удалять ноду в down!!
-// release, т.к. вызывается после "обработки ноды", т.е. сначала был проход слева-направо (side),
-// а потом справа-налево (release – отпускаем элемент (его можно удалять))
 void traversePostOrder(Node *root, Fun down, Fun side, Fun up, void *context);
 
 void unboundNode(Node *from, Node *target);
